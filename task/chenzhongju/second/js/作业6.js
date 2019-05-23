4a6630dc52c0159d09e48847c6e89d7ac9eecfa8
chenzhongju <2451493948@qq.com>
2019-05-24 00:29:08 +0800

    task

--- task/chenzhongju/second/js/作业6.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-   var ali=$('#a41 ul li') ;
-   var aDiv=$('.tabcontent div');
-   ali.click(function(){
-    $(this).addClass('cu').siblings().removeClass('cu');
-    var index=$(this).index(); 
-    aDiv.eq(index).show().siblings().hide();
-   });
-});
