5aacfd7eaa2a086a940b28e329791d2b670078cb
chenzhongju <2451493948@qq.com>
2019-05-23 18:24:03 +0800

    task

--- forth/INTdbconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-//公共信息配置文件
-
-//数据库信息配
-define("HOST","localhost"); //主机名
-define("USER","root");		//用户名
-define("PASS","chenzhongju");		//密码
-define("DBNAME","introduce");	//数据库名
--- forth/loginout.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-echo "<script>alert('你已退出！');
-location.href='登录.html';</script>";
-
-?>
